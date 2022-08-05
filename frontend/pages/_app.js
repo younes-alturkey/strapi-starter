fecc6e4244937a756cb3684f5d40b1a01f6b13bc
Younes Alturkey <hi@younes.ninja>
2022-08-05 20:20:57 +0300

    finish custom features

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import App from "next/app"
 import Head from "next/head"
-import "styles/global.css"
+import "../styles/globals.css"
 import { createContext } from "react"
 import { fetchAPI } from "../lib/api"
 import { getStrapiMedia } from "../lib/media"
